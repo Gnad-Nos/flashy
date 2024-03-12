0bcdc921b657794322b9a967f05846366f85700a
Gnad-Nos <sondhh3052001@gmail.com>
2024-03-12 20:49:19 +0700

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <div>a</div>
+      <div>a</div>
+      <div>a</div>
+      <div>a</div>
+      <div>a</div>
+      <div>a</div>
+      <div>a</div>
+    </div>
+  );
+}
+
+export default page;
