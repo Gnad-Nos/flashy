d03a0cbeff4eee68c8398229e315d58072cb51df
chuheongungay <ductam2943@gmail.com>
2024-03-24 15:50:05 +0700

    feat(config): add husky,eslint, commitlint, prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 
+/**
+ * this is home component
+ * @returns none
+ */
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
